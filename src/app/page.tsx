e2f67fee2db5403e907885ee438d920861db8d84
Srejith Ramesh <srejithramesh@gmail.com>
2025-11-07 21:17:05 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
-import CTA from "@/components/CTA";
 import Waitlist from "@/components/WaitlistForm";
 
 const HomePage: React.FC = () => {
