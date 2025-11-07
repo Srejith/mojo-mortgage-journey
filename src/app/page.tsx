19376c8f938ba8969ed3ba7d5e1dacd60d77ebc5
Srejith Ramesh <srejithramesh@gmail.com>
2025-11-07 20:57:42 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Hero from "@/components/Hero";
-import Testimonials from "@/components/Testimonials";
-import Pricing from "@/components/Pricing/Pricing";
-import FAQ from "@/components/FAQ";
-import Logos from "@/components/Logos";
 import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
 import Section from "@/components/Section";
 import Stats from "@/components/Stats";
 import CTA from "@/components/CTA";
+import Waitlist from "@/components/WaitlistForm";
 
 const HomePage: React.FC = () => {
   return (
@@ -16,27 +13,13 @@ const HomePage: React.FC = () => {
       <Logos />
       <Container>
         <Benefits />
-
         <Section
           id="pricing"
           title="Pricing"
           description="Simple, transparent pricing. No surprises."
         >
-          <Pricing />
         </Section>
-
-        <Section
-          id="testimonials"
-          title="What Our Clients Say"
-          description="Hear from those who have partnered with us."
-        >
-          <Testimonials />
-        </Section>
-
-        <FAQ />
-
-        <Stats />
-        
+        <Waitlist/>
         <CTA />
       </Container>
     </>
