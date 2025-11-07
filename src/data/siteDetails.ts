fa254c3e7571fdb1faf0fd4d352302e217fbe78c
Srejith Ramesh <srejithramesh@gmail.com>
2025-11-07 20:47:03 +0000

    Update siteDetails.ts

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,5 +1,12 @@
 export const siteDetails = {
     siteName: "MoJo - Mortgage Journey",
     siteDescription: "Your trusted partner in navigating the mortgage journey",
-    siteUrl: "https://mojo-mortgage.com", // Update with your actual domain
+    metadata: {
+        title: 'MoJo - Mortgage Journey',
+        description: 'Your trusted partner in navigating the mortgage journey',
+    },
+    language: 'en-us',
+    locale: 'en-US',
+    siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // or use a string for the logo e.g. "TechStartup"
+    siteUrl: "https://mojo-mortgage-journey.vercel.app/", // Update with your actual domain
 }
