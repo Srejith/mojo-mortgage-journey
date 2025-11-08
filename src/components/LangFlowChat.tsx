0b224eabe1faa857914ee0b20399a7eb535acbd3
Srejith Ramesh <srejithramesh@gmail.com>
2025-11-08 10:44:48 +0000

    Add files via upload

--- src/components/LangFlowChat.tsx
+++ src/components/LangFlowChat.tsx
@@ -42,6 +42,7 @@ const LangFlowChat = () => {
       <langflow-chat
         host_url={langFlowChat.hostUrl}
         flow_id={langFlowChat.flowId}
+        api_key={langFlowChat.apikey}
         chat_position={"top-right"}
         start_open={true}
         width={800}
